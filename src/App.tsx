20a5c8f803e2f82d4679ec26c85fc363bc915554
Jorge Munera <gerjo9211@hotmail.com>
2025-05-18 11:11:57 -0500

    init: estructura inicial del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center text-xl font-semibold">
+      Empresa de envíos
+    </div>
+  );
+}
+
+export default App;
